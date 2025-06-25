72bd1f9553885d067468b81fb020d00f87223da2
jdc4life <jesus.d.work@gmail.com>
2025-06-24 22:01:38 -0400

    Initial upload of starter React + Tailwind app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-center p-8">
+      <h1 className="text-3xl font-bold">Drama Llamas App</h1>
+      <p className="text-lg mt-4">Welcome to the NFT voting and engagement hub.</p>
+    </div>
+  );
+}
+
+export default App;
